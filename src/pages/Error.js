018ebf00b5f8b446ba0ea986c48937ca01e27d50
David Anderson <david.anderson1993@gmail.com>
2022-04-25 14:33:58 -0500

    updating 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "./errorPage.css";
+import LoadingButton from "../components/ButtonLoadingState";
 
 function Error() {
-  return <div>404 Error Page</div>;
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>404 Error</h1>
+        <h4>Page not found 🔎</h4>
+        <p className="infoText">
+          The link you clicked may be broken or the page may have been
+          removed...
+        </p>
+        <LoadingButton>Take Me Home!</LoadingButton>
+      </div>
+    </div>
+  );
 }
-
 export default Error;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,20 @@ import { Container, Row, Col } from "react-bootstrap";
 
 function Home() {
   return (
-    <Container className="App-header">
-      <Row>
-        <Col>
-          <h1>HOME PAGE</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col sm>1</Col>
-        <Col sm>2</Col>
-        <Col sm>3</Col>
-      </Row>
-    </Container>
+    <div className="App App-header">
+      <Container>
+        <Row>
+          <Col>
+            <h1>HOME PAGE</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm>1</Col>
+          <Col sm>2</Col>
+          <Col sm>3</Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
